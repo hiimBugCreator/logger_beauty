aa78972de640f0e73b67058c3d5686ba3b51946c
Vu Huy Loc <vhuyloc@Vus-iMac-Pro.local>
2024-01-04 17:28:14 +0700

    Create LogColor class

---
+++ lib/log_color.dart
@@ -0,0 +1,17 @@
+class LogColor {
+  static const String reset = '\x1B[0m';
+  static const String red = '\x1B[31m';
+  static const String green = '\x1B[32m';
+  static const String yellow = '\x1B[33m';
+  static const String blue = '\x1B[34m';
+  static const String magenta = '\x1B[35m';
+  static const String cyan = '\x1B[36m';
+  static const String white = '\x1B[37m';
+  static const String lightRed = '\x1B[91m';
+  static const String lightGreen = '\x1B[92m';
+  static const String lightYellow = '\x1B[93m';
+  static const String lightBlue = '\x1B[94m';
+  static const String lightMagenta = '\x1B[95m';
+  static const String lightCyan = '\x1B[96m';
+  static const String lightWhite = '\x1B[97m';
+}
